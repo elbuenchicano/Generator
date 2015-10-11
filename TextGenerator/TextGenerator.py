82e45f3c7ed004b1f238114fb6fef126f672a745
rensso <soulchicano@gmail.com>
2015-10-11 17:22:47 -0300

    running all

--- TextGenerator/TextGenerator.py
+++ TextGenerator/TextGenerator.py
@@ -54,104 +54,111 @@ def updateInfo(dir):
         fo.close()
 
 ################################################################################
-def colectInfo(key , valu):
+def colectInfo(valu):
     out = ""
     pos = 0
     for name in sorted(dictionary):
         valf = Gentries[pos].get()
-        if name == key:
-            valf = str(valu)
+
+        valf = valf.replace("{i}",str(valu))
         out += name + ": " + valf + "\n"   
         pos += 1
     return out
 #def updateinfo
+#################################################################################
+def save_olds() :
+    fo      = open("./saveold", "w")
+    fo.write(entry.get())
+    fo.write(entry3.get())
+    fo.close()
+#def save_olds
 ################################################################################
-def addEntry () :
-    pos = 0
-    out = ""
-    batOut = "@echo off\n"
-    bounds = []
-    key = ""
-    for name in sorted(dictionary):
-        val = Gentries[pos].get()
-        if val[0] == "\"":
-            out += name + ": " + val + "\n"
-        else:
-            if val.find(":") > 0:
-                bounds  = val.split(":") 
-                key     = name
-                break 
-            else:
-                out += name + ": " + val + "\n"    
-        pos += 1
+def generate() :
+    out     = ""
+    batOut  = "@echo off\n"
+    pos     = 0
+    fexe   = entry.get().replace("\n"," ")
+    outbat  = entry3.get() + "/" + entry2.get() + ".bat"
+     
+    try:
 
-    outbat  = entry.get() + "/" + entry2.get() + ".bat"
-
-    if len(bounds) > 0:
-        try:
-            out_fileb = open(outbat, "w")
+        out_fileb = open(outbat, "w")
+        out_fileb.write(batOut)
+        for x in range( int( ent_ini.get() ), int(  ent_fin.get() )+1, int( ent_incr.get() ) ):
+            outyml  = entry3.get() + "/" + entry4.get() + str(x) +".yml"
+            batOut  = fexe + " " + outyml + "\n"  
             out_fileb.write(batOut)
-            for x in range(int(bounds[0]),int(bounds[2])+1,int(bounds[1])):
-                outyml  = entry3.get() + "/" + entry4.get() + str(x) +".yml"
-                batOut  = execName + " " + outyml + "\n"  
-                out_fileb.write(batOut)
 
-                out = colectInfo(key,x)
-                out_filey = open(outyml, "w")
-                out_filey.write(out)
-                out_filey.close()
-            out_fileb.close()
-            messagebox.showinfo("info", "All Saved")
-        except:
-            messagebox.showinfo("info", "No Info")   
-    else:
-        try:
-            outyml  = entry3.get() + "/" + entry4.get() + ".yml"
-            batOut  += execName + " " + outyml   
-            out_filey = open(outyml, "w") 
+            out = colectInfo(x)
+            out_filey = open(outyml, "w")
             out_filey.write(out)
-            out_fileb = open(outbat, "w")
-            out_fileb.write(batOut)
-            out_fileb.close()
             out_filey.close()
-            messagebox.showinfo("info", "Saved")
-        except:
-            messagebox.showinfo("info", "No Info")
-#def addentry
-################################################################################
+        out_fileb.close()
+        messagebox.showinfo("info", "All Saved")
+    except:
+        messagebox.showinfo("info", "No Info")   
+    save_olds()
+#def generate
+#################################################################################
+def load_olds() :
+    fo      = open("./saveold", "r")
+    entry.insert(0,fo.readline())
+    entry3.insert(0,fo.readline()) 
+    fo.close()
+
+#def save_vals
+#################################################################################
 def createHeader(frame) :
 
-    global entry, entry2, entry3, entry4
+    global entry, entry2, entry3, entry4, ent_ini, ent_fin, ent_incr
+
     entry = Entry(frame, width=30)
     entry.grid(row = 0, column =0)
-    entry2 = Entry(frame, width=20)
-    
-    entry2.grid(row = 0, column =1)
 
+    entry2 = Entry(frame, width=10)
+    entry2.grid(row = 0, column =2)
+    entry2.insert(0,"batOut")
 
-    b1 = Button(frame,text="Dir .bat",command=load_file, width = 10).grid(row = 0, column =2)
-    b2 = Button(frame,text="Generate",command=addEntry, width = 10).grid(row = 0, column =3, rowspan = 2, sticky='ns')
+    b1 = Button(frame,text="File .exe",command=load_file, width = 10).grid(row = 0, column =1)
+    b2 = Button(frame,text="Generate",command=generate, width = 10).grid(row = 0, column =3, rowspan = 2, sticky='ns')
     
     entry3 = Entry(frame, width=30)
     entry3.grid(row = 1, column =0)
     
-    entry4 = Entry(frame, width=20)
-    entry4.grid(row = 1, column =1)
-    
-    b3 = Button(frame,text="Dir store",command=load_file2, width = 10).grid(row = 1, column =2)
+    entry4 = Entry(frame, width=10)
+    entry4.grid(row = 1, column =2)
+    entry4.insert(0,"test")
+
+    b3 = Button(frame,text="Dir store",command=load_file2, width = 10).grid(row = 1, column =1)
     #return 
+
+    Label(frame, text="Ini - Incr - Fin : {i}").grid(row=3, column=0, sticky=W, padx = 10, pady =2)
+    ent_ini = Entry(frame, width=10)
+    ent_ini.grid(row = 3, column =1)
+    ent_incr = Entry(frame, width=10)
+    ent_incr.grid(row = 3, column =2)
+    ent_fin = Entry(frame, width=10)
+    ent_fin.grid(row = 3, column =3)
+
+    ent_ini.insert(0,"1")
+    ent_incr.insert(0,"1")
+    ent_fin.insert(0,"1")
+
+    load_olds()
+    
+    
 
 def createGrid(frame):
     
     createHeader(frame)   
 
     #-----------------------------------------------------------------------------
-    cont = 3
+    #cont = 4
     
-    Label(frame, text="").grid(row=cont, column=0, sticky=W, padx = 10, pady =2)
-    Label(frame, text="").grid(row=cont, column=1, sticky=W, padx = 10, pady =2)
+    #Label(frame, text="").grid(row=cont, column=0, sticky=W, padx = 10, pady =2)
+    #Label(frame, text="").grid(row=cont, column=1, sticky=W, padx = 10, pady =2)
     
-    cont += 1
+    cont = 4
 
     Label(frame, text="Variables").grid(row=cont, column=0, sticky=W, padx = 10, pady =2)
     Label(frame, text="Values").grid(row=cont, column=1, sticky=W, padx = 10, pady =2)
@@ -160,10 +167,10 @@ def createGrid(frame):
         Label(frame, text=name).grid(row=cont, column=0, sticky=W, padx = 10, pady = 2)
         nameVar = StringVar(value = dictionary[name])
         Gtextvars.append(nameVar)
-        entryg = Entry(frame, textvariable= nameVar, width=40)
-        entryg.grid(row=cont, column=1, sticky=W, columnspan = 2)
+        entryg = Entry(frame, textvariable= nameVar, width=35)
+        entryg.grid(row=cont, column=1, sticky=W, columnspan = 3)
         Gentries.append(entryg)
-        Label(frame, text="").grid(row=cont, column=3, sticky=W, padx = 5, pady =3)
+        Label(frame, text="").grid(row=cont, column=6, sticky=W, padx = 5, pady =3)
         cont    += 1
     #for
     Label(frame, text="").grid(row=cont, column=0, sticky=W, padx = 10, pady =2)
@@ -171,7 +178,7 @@ def createGrid(frame):
 
 ################################################################################
 def load_file():
-    fname = tkFileDialog.askdirectory()
+    fname = tkFileDialog.askopenfilename()
     if fname !="":
         #try:
         print(fname)
@@ -211,8 +218,7 @@ class VerticalScrolledFrame(Frame):
                              yscrollcommand=vscrollbar.set)
         canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
         vscrollbar.config(command=canvas.yview)
-        canvas.config(width=200,height=650)
-
+        canvas.config(width=200,height=700)
         # reset the view
         canvas.xview_moveto(0)
         canvas.yview_moveto(0)
